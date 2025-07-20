8f9e718636bfa86830fb6a26de2f56e7750764e5
Raj Kothari <54576688+rajkothari634@users.noreply.github.com>
2025-07-20 19:14:26 +0530

    Update ISPExample.java

--- ISPExample.java
+++ ISPExample.java
@@ -1,4 +1,23 @@
-// Interface Segregation Principle (ISP)
+// ✅ INTERFACE SEGREGATION PRINCIPLE (ISP)
+// No client should be forced to depend on methods it does not use.
+
+//// ❌ Bad Example: Robot forced to implement eat() even if it doesn’t need it.
+interface Worker {
+    void work();
+    void eat();
+}
+
+class Robot implements Worker {
+    public void work() {
+        System.out.println("Robot working");
+    }
+
+    public void eat() {
+        throw new UnsupportedOperationException("Robots don't eat!");
+    }
+}
+
+//// ✅ Good Example: Separate interfaces for separate responsibilities.
 
 interface Workable {
     void work();
@@ -12,12 +31,13 @@ class Human implements Workable, Eatable {
     public void work() {
         System.out.println("Human working");
     }
+
     public void eat() {
         System.out.println("Human eating");
     }
 }
 
-class Robot implements Workable {
+class Robot2 implements Workable {
     public void work() {
         System.out.println("Robot working");
     }
